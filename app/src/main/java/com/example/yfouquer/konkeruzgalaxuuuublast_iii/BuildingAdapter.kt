50f6c766f007355b37c665adb22543b71d7b71f4
yfo <yohann.fouquer@lesfurets.com>
2018-02-20 17:56:38 +0100

    :sparkles: Register Login

--- app/src/main/java/com/example/yfouquer/konkeruzgalaxuuuublast_iii/BuildingAdapter.kt
+++ app/src/main/java/com/example/yfouquer/konkeruzgalaxuuuublast_iii/BuildingAdapter.kt
@@ -4,13 +4,16 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.yfouquer.konkeruzgalaxuuuublast_iii.MainActivity.Companion.DataBuild
+import com.example.yfouquer.konkeruzgalaxuuuublast_iii.R.layout.view_row
 import kotlinx.android.synthetic.main.view_row.view.*
 
-class BuildingAdapter(private var mDataset: List<Pair<String,Int>>) : RecyclerView.Adapter<BuildingAdapter.ViewHolder>() {
+class BuildingAdapter(private var mDataset: List<DataBuild>) : RecyclerView.Adapter<BuildingAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.text.text = mDataset[position].first
-        holder.image.setImageResource(mDataset[position].second)
+        holder.text.text = mDataset[position].name
+        holder.image.setImageResource(mDataset[position].imageRef)
+        holder.button.text = "Metal : ${mDataset[position].metPrice}\n Cristal : ${mDataset[position].cristPrice}"
     }
 
     override fun getItemCount(): Int {
@@ -18,12 +21,13 @@ class BuildingAdapter(private var mDataset: List<Pair<String,Int>>) : RecyclerVi
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.view_row, parent, false)
+        val v = LayoutInflater.from(parent.context).inflate(view_row, parent, false)
         return ViewHolder(v)
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        val text = view.textView
-        val image = view.imageView
+        val text = view.textView!!
+        val image = view.imageView!!
+        val button = view.button!!
     }
 }
--- app/src/main/java/com/example/yfouquer/konkeruzgalaxuuuublast_iii/MainActivity.kt
+++ app/src/main/java/com/example/yfouquer/konkeruzgalaxuuuublast_iii/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.yfouquer.konkeruzgalaxuuuublast_iii
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,9 +15,17 @@ class MainActivity : AppCompatActivity() {
         recycler_view.setHasFixedSize(true)
         recycler_view.layoutManager = LinearLayoutManager(this)
 
-        recycler_view.adapter = BuildingAdapter(
-                listOf(Pair("Mine de metal", R.drawable.mine_de_metal),
-                        Pair("Mine de cristal", R.drawable.mine_de_cristal)))
+        recycler_view.adapter = BuildingAdapter(data)
+
+
+    }
+
+    companion object {
+        data class DataBuild(val name: String, val imageRef: Int, val metPrice: Int, val cristPrice: Int)
+
+        val data = listOf(
+                DataBuild("Mine de metal", R.drawable.mine_de_metal, 100, 0),
+                DataBuild("Mine de cristal", R.drawable.mine_de_cristal, 200, 300))
 
 
     }
